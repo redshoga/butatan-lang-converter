e27771e6995392727bd5efcade2c99099462afc1
redshoga <yui.ichioka.contact@gmail.com>
2020-03-08 18:11:46 +0900

    Updates

---
+++ precache-manifest.3d79a9af329e25db5cc9e03d82219789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a20a6cd82be98a327ed0178afa84da0",
+    "url": "/butatan-lang-converter/index.html"
+  },
+  {
+    "revision": "bda564b18b488b7ae5e9",
+    "url": "/butatan-lang-converter/static/css/main.42b9ca2b.chunk.css"
+  },
+  {
+    "revision": "c8e3987ed140c75ee495",
+    "url": "/butatan-lang-converter/static/js/2.a59fe167.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/butatan-lang-converter/static/js/2.a59fe167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda564b18b488b7ae5e9",
+    "url": "/butatan-lang-converter/static/js/main.b2788533.chunk.js"
+  },
+  {
+    "revision": "f03c7a8221a67dab1c00",
+    "url": "/butatan-lang-converter/static/js/runtime-main.1c9ef0d1.js"
+  }
+]);
